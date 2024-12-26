c4782ff833664ff06024a67a3448f3ee68cac95c
Noah Black <bjacknoah@gmail.com>
2024-12-26 18:44:42 +0500

    Clear unused strings

--- models/user.go
+++ models/user.go
@@ -1,13 +1,12 @@
 package models
 
 type SignUpInput struct {
-	Email    string `json:"email" binding:"required"`
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
 type User struct {
+	SignUpInput
 	Id      int  `json:"-" db:"id"`
 	IsAdmin bool `json:"-" db:"is_admin"`
-	SignUpInput
 }
